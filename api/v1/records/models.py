64f9d29092f881d492efcb0e47b987b1a0a59692
Jeffrey Adorkor <adorkorj@gmail.com>
2022-04-08 13:26:39 +0000

    added medical record model

--- api/v1/records/models.py
+++ api/v1/records/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from api.v1.users.models import Patient, Doctor
+from api.v1.bookings.models import Booking
 
 # Create your models here.
 class MedicalRecord(models.Model):
@@ -9,3 +10,17 @@ class MedicalRecord(models.Model):
 
     def __str__(self):
         return f"{self.patient}'s Medical Record"
+
+class RecordItem(models.Model):
+    medical_record = models.OneToOneField(MedicalRecord, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    booking_id = models.ForeignKey(Booking, null=True, on_delete=models.SET_NULL)
+    diagnosis = models.TextField()
+    recommendation = models.TextField()
+    medication = models.TextField()
+    face_analysis = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.medical_record} - Record {self.id}"
